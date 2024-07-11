3f6d1d0695c2e5ff1223dbaacf03e2203915d146
unknown <monikapatidar1009@gmail.com>
2024-07-11 13:02:00 +0530

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+const Navigation = () => {
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="https://raw.githubusercontent.com/anshuopinion/React-10-Projects/e415c1815084c236e5fb39ffcd4d9d5587c436eb/project-1/public/images/brand_logo.png" />
+      </div>
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
+
+      <button>login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
